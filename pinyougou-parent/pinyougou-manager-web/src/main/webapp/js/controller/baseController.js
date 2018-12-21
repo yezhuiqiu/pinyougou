3296ff777735a97a9ba969cab182cfdd68874fa7
001 <001@DESKTOP-S9N4L6I>
2018-12-21 16:48:19 +0800

    提交

---
+++ pinyougou-parent/pinyougou-manager-web/src/main/webapp/js/controller/baseController.js
@@ -0,0 +1,34 @@
+app.controller('baseController',function($scope){
+	
+	//分页控件配置 
+	$scope.paginationConf = {
+			 currentPage: 1,
+			 totalItems: 10,
+			 itemsPerPage: 10,
+			 perPageOptions: [10, 20, 30, 40, 50],
+			 onChange: function(){
+			        	 $scope.reloadList();//重新加载
+			 }
+	}; 
+	
+ 	$scope.reloadList=function(){
+			 //切换页码  
+			 $scope.search( $scope.paginationConf.currentPage, $scope.paginationConf.itemsPerPage);
+  		
+  	}
+	
+  	$scope.selectIds=[];//用户选中的id集合
+   	
+    $scope.updateSelection=function($event,id){
+   		
+   		if($event.target.checked){
+   			$scope.selectIds.push(id);//push方法向集合添加元素
+   		}else{
+       		var index=$scope.selectIds.indexOf(id);//查找值的位置
+       		$scope.selectIds.splice(index,1);//参数1是移除的位置，参数2是移除的个数
+   		}
+   		
+    
+   	} 
+	
+});
